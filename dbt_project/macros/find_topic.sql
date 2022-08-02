e7219cec768b72a22649b868683ed186fae024bf
Kai Sahling <kaisahling@gmail.com>
2022-08-02 11:41:05 +0200

    fix find topic

--- dbt_project/macros/find_topic.sql
+++ dbt_project/macros/find_topic.sql
@@ -1,6 +1,6 @@
 {% macro find_topic(topics, value_column) %}
     {% for topic in topics %}
-    sum(case when REGEXP_CONTAINS(LOWER({{value_column}}), r'{{topic}}') THEN 1
-    ELSE 0 END) as {{topic.replace(" ","_")}}_mentioned,
+    sum(CASE WHEN REGEXP_CONTAINS(LOWER({{value_column}}), r'{{topic}}') THEN 1
+    ELSE 0 END) AS {{topic.replace(" ","_")}}_mentioned,
    {% endfor %}
 {% endmacro %}
